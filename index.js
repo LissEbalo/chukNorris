186baf18cae0970b570e5c8ae2fdd4fa2957a388
Lissette Lillo <xlissx@gmail.com>
2024-10-15 19:14:37 -0300

    proyecto

---
+++ index.js
@@ -0,0 +1,9 @@
+async function cambiarFrase() {
+    const respuesta = await fetch("https://api.chucknorris.io/jokes/random")
+    const data = await respuesta.json()
+    console.log(data);
+    let phrase_div = document.getElementById("reload");
+    phrase_div.innerHTML = data.value;
+}
+cambiarFrase()
+
